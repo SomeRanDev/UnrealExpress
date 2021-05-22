5aa152d736e9eeaed3ff5cdfeee4ca3c20f8e168
Robert Borghese <28355157+RobertBorghese@users.noreply.github.com>
2021-05-22 05:45:59 -0400

    Initial Commit
    Implemented uinit
    Implemented incomplete printing macros

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# UnrealExpress
- Functions and macros that drastically ease the amount of typing and memorization required for common functionality in Unreal/C++.
+# Unreal Express
+
+Functions and macros that drastically ease the amount of typing and memorization required for common functionality in Unreal/C++.
--- UnrealEasyPrint.h
+++ UnrealEasyPrint.h
@@ -1,9 +1,59 @@
 #pragma once
 
-#define HBShow(str) if(GEngine) GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT(str)));
-#define HBShow1(str, num) if(GEngine) GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT(str), num));
-#define HBShow2(str, num1, num2) if(GEngine) GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT(str), num1, num2));
-#define HBShow3(str, num1, num2, num3) if(GEngine) GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT(str), num1, num2, num3));
-#define HBShow5(str, num1, num2, num3, num4, num5) if(GEngine) GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, FString::Printf(TEXT(str), num1, num2, num3, num4, num5));
+#define uelog UnrealEasyPrintImpl<false, 0>()
+#define uewarn UnrealEasyPrintImpl<false, 1>()
+#define ueerror UnrealEasyPrintImpl<false, 2>()
 
-#define HBPrint5(str, num1, num2, num3, num4, num5) UE_LOG(LogTemp, Warning, TEXT(str), num1, num2, num3, num4, num5);
+#define ueshow UnrealEasyPrintImpl<true, 0>()
+#define ueshowwarn UnrealEasyPrintImpl<true, 1>()
+#define ueshowerror UnrealEasyPrintImpl<true, 2>()
+
+template<bool IsShow, int Level>
+class UnrealEasyPrintImpl {
+public:
+	UnrealEasyPrintImpl() {}
+
+	~UnrealEasyPrintImpl() {
+		if(!Output.IsEmpty()) {
+			constexpr bool IsLog = Level == 0;
+			constexpr bool IsWarning = Level == 1;
+			constexpr bool IsError = Level == 2;
+			if constexpr(IsShow) {
+				if(GEngine) {
+					constexpr FColor Color = IsLog ? FColor::White : (IsWarning ? FColor::Yellow : FColor::Red);
+					GEngine->AddOnScreenDebugMessage(-1, 5.f, Color, Output);
+				}
+			} else {
+				if constexpr(IsLog) UE_LOG(LogTemp, Log, TEXT("%s"), *Output);
+				if constexpr(IsWarning) UE_LOG(LogTemp, Warning, TEXT("%s"), *Output);
+				if constexpr(IsError) UE_LOG(LogTemp, Error, TEXT("%s"), *Output);
+			}
+		}
+	}
+
+	void AddText(const FString& Input) {
+		Output += (Output.IsEmpty() ? "" : " ") + Input;
+	}
+	
+	UnrealEasyPrintImpl& operator<<(const int Input) {
+		AddText(FString::FromInt(Input));
+		return *this;
+	}
+
+	UnrealEasyPrintImpl& operator<<(const float Input) {
+		AddText(FString::SanitizeFloat(Input));
+		return *this;
+	}
+
+	UnrealEasyPrintImpl& operator<<(const bool Input) {
+		AddText(Input ? TEXT("true") : TEXT("false"));
+		return *this;
+	}
+	
+	UnrealEasyPrintImpl& operator<<(const FVector& Input) {
+		AddText(Input.ToString());
+		return *this;
+	}
+	
+	FString Output;
+};
